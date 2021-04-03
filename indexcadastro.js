9ad493df0f5758438e12b5d6f62b249560299a70
Adilson Pinheiro de Carvalho Junior <79886778+Adilson-Junior-ADS@users.noreply.github.com>
2021-04-03 19:53:24 -0400

    Add files via upload

---
+++ indexcadastro.js
@@ -0,0 +1,7 @@
+function submit_form_login(){
+
+} 
+   if (document.form_login.input_email.value == 'imperativo77@gmail.com'){
+   alert("Parabéns agora você faz parte do fan clube do willianzinho!");
+   }
+}   
